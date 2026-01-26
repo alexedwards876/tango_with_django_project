cd582d1ad5a4b6f17324a17aea71dd8cb2cb0b07
Alex Edwards <3017116e@student.gla.ac.uk>
2026-01-26 22:17:36 +0000

    8.1 + 8.2 + 8.3 + 8.4 + 8.5 + 8.6

--- rango/views.py
+++ rango/views.py
@@ -26,17 +26,20 @@ def about(request):
 
 def show_category(request, category_name_slug):
     context_dict = {}
-
+    
     try:
         category = Category.objects.get(slug=category_name_slug)
+        
         pages = Page.objects.filter(category=category)
+        
         context_dict['pages'] = pages
         context_dict['category'] = category
-
+        
     except Category.DoesNotExist:
+        
         context_dict['category'] = None
         context_dict['pages'] = None
-
+    
     return render(request, 'rango/category.html', context=context_dict)
 
 def add_category(request):
